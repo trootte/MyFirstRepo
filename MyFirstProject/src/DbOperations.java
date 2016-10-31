7f9f02748a948987639c2b31932f77e9282ab974
Trond <Trond@macbook-pro-4.localdomain>
2016-10-31 14:02:29 +0100

    161031_1

--- MyFirstProject/src/DbOperations.java
+++ MyFirstProject/src/DbOperations.java
@@ -1,20 +1,139 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbOperations {	
-	static String databaseName;
 	
-	DbOperations (String dbName) {
-		databaseName = dbName;
+	private Connection conn;
+	
+	
+	DbOperations (String dataBaseName, String[] tableArray) {
+		try {
+			// initialize connection
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/sys?" +
+                    "user=scrape_user&password=scrape_pwd");
+			
+			try {
+				conn.setSchema(dataBaseName);
+				if (conn.getSchema() == null) conn.setSchema("sys");
+				System.out.println("DbOperations Construct getschema : " + conn.getSchema());
+			}
+			catch (SQLException e){
+				System.out.print("DbOperations Construct SqlException : " + e.getMessage()); 
+			}
+			
+			createDbIfNotExists(conn, dataBaseName);
+			createTablesIfNotExists(conn, tableArray);
+			
+		} catch (SQLException ex) {
+		    // handle any errors
+			System.out.println("getConnection() Error");
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
+	
+	public Connection getConnection() {
+		return conn;
+	}
+	
+	public static boolean createDbIfNotExists(Connection conn, String dataBaseName) {
+		try {
+		    String sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = \"" + dataBaseName + "\"";
+		    
+		    Statement stmt = conn.createStatement();
+		    stmt.executeQuery(sql);
+		    
+		    ResultSet rs = stmt.getResultSet();
+		    
+		    if (!rs.next()) {
+		    	sql = "CREATE SCHEMA `" + dataBaseName + "` DEFAULT CHARACTER SET latin1 COLLATE latin1_danish_ci ;"; 
+		    	
+		    	stmt.execute(sql);
+		    	//TODO catch
+		    }
+		    
+		    rs.close();
+		    stmt.close();
+		    
+		    conn.setSchema(dataBaseName);
+		    System.out.println("createDb getSchema : " + conn.getSchema());
+		    
+		} catch (SQLException ex) {
+		    // handle any errors
+			System.out.println("createDbIfNotExists() Error");
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		return true;
+	}
+	
+	public static boolean createTablesIfNotExists (Connection conn, String[] tableArray) {
+		//TODO legg statements i filer/config
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "";
+			System.out.println("createTables Schema : " + conn.getSchema());
+			for (int i = 0; i < tableArray.length; i++) {
+				sql = "SELECT IF (EXISTS (" +
+						"select 1 from information_schema.TABLES where table_schema = \"" + conn.getSchema() + "\" and table_name = \"" + tableArray[i] + "\"),1 ,0)";
+				
+				stmt.execute(sql);
+				ResultSet rs = stmt.getResultSet();
+				
+				while (rs.next()) {
+					if (rs.getString(1).equals("0")) {
+						
+						switch (tableArray[i]) {
+							case "sites" :
+								sql = "CREATE TABLE `" + conn.getSchema() + "`.`Sites` (" +
+										  "`id` INT NOT NULL AUTO_INCREMENT," +
+										  "`url` VARCHAR(45) NOT NULL," +
+										  "`https` VARCHAR(45) NOT NULL," +
+										  "PRIMARY KEY (`id`)," +
+										  "UNIQUE INDEX `id_UNIQUE` (`id` ASC))";
+								break;
+							case "lines" : 
+								sql = "CREATE TABLE `" + conn.getSchema() + "`.`Lines` (" +
+										"`id` INT NOT NULL AUTO_INCREMENT," +
+										"`siteId` INT NOT NULL," +
+										"`text` VARCHAR(255) NULL," +
+										"PRIMARY KEY (`id`)," +
+										"UNIQUE INDEX `id_UNIQUE` (`id` ASC));";
+								break;
+							default : sql = "";
+						}
+					}
+				}
+				
+				stmt.execute(sql);
+				
+			}
+						
+			stmt.close();
+			
+		}
+		catch (SQLException ex){
+		    // handle any errors
+			System.out.println("createTablesIfNotExists() Error");
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		}
 		
-		System.out.println(dbName);
+		return true;
+	}
+	
+	public static boolean appendSiteToDB(Connection conn) {
+		return true;
 	}
 	
 	public static boolean appendLineToDB(String readerLine) {
 		System.out.println(readerLine);
-		return true;
-	}
-	
-	public static boolean createDbIfNotExists() {
-		System.out.println(databaseName);
 		return true;
 	}
 }
--- MyFirstProject/src/WebScraper.java
+++ MyFirstProject/src/WebScraper.java
@@ -1,21 +1,39 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.util.Enumeration;
+import java.util.Properties;
 
 public class WebScraper {
 
+	private static String dataBaseName; 
+	private static String url;
+	private static String[] tableArray;
+	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub test
+		if (args.length > 0) {
+			if (args[0].equals("1")) {
+				System.out.println("Main : write config!");
+				writeConfig();
+			}
+		}
 		
-		String url = "http://vg.no";
-		
-		String returnValue = scrapeWebPage(url);
-		System.out.println("Start...");
-		System.out.println(returnValue);
-		System.out.println("End...");
+		if (readConfig()) {
+			DbOperations DbOp = new DbOperations(dataBaseName, tableArray);
+			//Connection webScraperConnection = DbOp.getConnection();
+			
+			//DbOperations.appendSiteToDB(webScraperConnection);
+		}
+		else {
+			System.out.println("main feil");
+		}
 	}
 	
 	private static String scrapeWebPage (String url) {
@@ -40,6 +58,73 @@ public class WebScraper {
 		return message;
 	}
 	
+	private static boolean readConfig() {
+		Properties prop = new Properties();
+		InputStream input = null;
+		boolean success = false;
+		try {
+			try {
+				input = new FileInputStream("config.properties");
+			}
+			catch (Exception e) {
+				System.out.println("readConfig feil : " + e.getMessage());
+				success = false;
+				return success;
+			}
+
+			prop.load(input);
+			dataBaseName = prop.getProperty("dataBaseName");
+			
+			// how many properties with tablenames? -> initialize tableArray
+			int j = 0;
+			Enumeration<?> e = prop.propertyNames();
+			
+			while (e.hasMoreElements()) {
+				String key = (String) e.nextElement();
+				if (key.startsWith("table")) j++;	
+				}
+			
+			tableArray = new String[j];
+			
+			//fetch tablenames
+			int i = 1;
+			
+
+
+			
+			String propertyTable = "table" + i;
+			String configTableName = prop.getProperty(propertyTable);
+			
+			
+			do {
+				tableArray[i-1] = configTableName;
+				i++;
+				propertyTable = "table" + i;
+				configTableName = prop.getProperty(propertyTable);
+			}
+			while (configTableName != null);
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			success = false;
+			return success;
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+					success = true;
+					return success;
+				} catch (IOException e) {
+					e.printStackTrace();
+					success = false;
+					return success;
+				}
+			}
+		}
+		success = true;
+		return success;
+	}
+	
 	private static boolean readLines(BufferedReader inReader) {
 		try {
 			while (inReader.readLine() != null) {
@@ -52,5 +137,36 @@ public class WebScraper {
 		}
 		
 		return true;
+	}
+	
+	
+	
+	private static void writeConfig() {
+		Properties prop = new Properties();
+		OutputStream output = null;
+
+		try {
+
+			output = new FileOutputStream("config.properties");
+
+			prop.setProperty("dataBaseName", "WebScraper");
+			prop.setProperty("url", "http://vg.no");
+			prop.setProperty("table1", "sites");
+			prop.setProperty("table2", "lines");
+			
+			prop.store(output, null);
+
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
 	}
 }
